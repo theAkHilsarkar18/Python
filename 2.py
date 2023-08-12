8764560e929e2d17eacbb1018c71d69ee5c60a23
akhil <akhilsodvadiya7@gmail.com>
2023-08-12 19:08:08 +0530

    12-08-2023

--- 2.py
+++ 2.py
@@ -56,3 +56,45 @@
 # y = 22
 # # print(x+y) # it will show erro
 # print(x.format(y))
+
+# to use multiple numbers into print string
+
+# age = 22
+# per = 90.67
+# intro = "My name is Akhil My age is {} and my percentage is {}"
+# intro = "My name is Akhil My age is {1} and my percentage is {0}"
+# also you can pass index number to fix value..
+# print(intro.format(age,per))
+
+# Escape sequence character
+
+# when you want to use "" into string it will be show error.
+
+# x = "My name is great "Akhil" sarkar" <- it will be show erro
+# x = "My name is the great \"Akhil Sarkar\" developer"
+# print(x)
+
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
+
+# a = "\'akhil\'"
+# print(a)
+# b = "akhil\\"
+# print(b)
+# c = "Hello\nWorld"
+# print(c)
+# d = "Hello\rWorld"
+# print(d)
+# e = "Hello\tWorld"
+# print(e)
+# f = "Hello\bWorld"
+# print(f)
+# g = "Hello\fWorld"
+# print(g)
