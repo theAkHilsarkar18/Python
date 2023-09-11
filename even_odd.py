74e4e21fd480fe7c7c470fe0f8ef8e205bfa0e65
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-11 23:58:27 +0530

    11-09-2023

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+# 4. Write a program that checks if a number is even or odd.
+
+x = input("Enter the value of x : ")
+x = int(x)
+if(x%2==0) :
+    print(x," is even")
+else: 
+    print(x," is odd")
---
+++ question_bank-1.py
@@ -0,0 +1,49 @@
+# Certainly! Here's a list of Python practice questions ranging from beginner to advanced levels to help you improve your programming skills:
+
+# **Beginner Level:**
+
+# 1. Write a Python program to print "Hello, World!" to the console.
+
+# 2. Create a program that asks the user for their name and then prints a greeting using their name.
+
+# 3. Calculate the area and perimeter of a rectangle with user-provided width and height.
+
+# 4. Write a program that checks if a number is even or odd.
+
+# 5. Create a simple calculator program that can perform basic arithmetic operations (addition, subtraction, multiplication, and division).
+
+# **Intermediate Level:**
+
+# 6. Write a Python function that returns the factorial of a given number.
+
+# 7. Implement a program to find the largest element in a list.
+
+# 8. Create a program that generates Fibonacci numbers up to a specified limit.
+
+# 9. Write a Python program to count the number of words in a text file.
+
+# 10. Implement a function that checks if a string is a palindrome (reads the same forwards and backwards).
+
+# **Advanced Level:**
+
+# 11. Implement a binary search algorithm to find a specific element in a sorted list.
+
+# 12. Create a program that generates all possible permutations of a list.
+
+# 13. Write a Python script to scrape data from a website using libraries like BeautifulSoup and requests.
+
+# 14. Implement a simple chatbot using natural language processing libraries like NLTK or spaCy.
+
+# 15. Create a program that simulates a basic banking system with classes for accounts, transactions, and balances.
+
+# 16. Build a web application using a Python web framework like Django or Flask.
+
+# 17. Implement a recommendation system based on user preferences and item similarities using collaborative filtering or content-based filtering.
+
+# 18. Develop a game like Tic-Tac-Toe or Blackjack in Python.
+
+# 19. Write a program that solves a Sudoku puzzle using backtracking.
+
+# 20. Build a RESTful API for a task management system using a Python framework like Flask or Django Rest Framework.
+
+# These questions cover a wide range of Python concepts and should help you progress from a beginner to an advanced level of proficiency. You can also modify and expand on these questions to create more complex projects and challenges as you continue to develop your Python skills.
