89e1c7d33d079b60cead1b1b96630cc85827a8aa
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-12 00:05:43 +0530

    12-09-2023

---
+++ simple_calc.py
@@ -0,0 +1,10 @@
+x = input("Enter value of x : ")
+y = input("Enter value of y : ")
+
+x = int(x)
+y = int(y)
+
+print("addition is : ",x+y)
+print("substraction is : ",x-y)
+print("multiplication is : ",x*y)
+print("division is : ",x/y)
