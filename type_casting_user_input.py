288d9fb0cc4719fbfc2ddf736386239f41578ca6
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-11 23:27:46 +0530

    11-09-2023

---
+++ type_casting_user_input.py
@@ -0,0 +1,14 @@
+
+x = input("Enter any value : ")
+
+# to convert into integer
+y = int(x)
+print(y+y)
+
+# to convert into float
+y = float(x)
+print(y+y)
+
+# to convert into string
+y = str(x)
+print(x+x)
