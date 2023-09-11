1628edc5e84c6c45d562f5b3fc766550a0912bde
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-11 23:22:40 +0530

    11-09-2023

--- 4.py
+++ 4.py
@@ -1,6 +1,14 @@
 # take user input in python
 # we can use input() method to take user input.
 # also we can pass message in input as input message.
-# python take string inout as basic input , we need to convert into another according our need.
+# python take string input as basic input , we need to convert into another according our need.
+
 x = input("Enter your name : ")
 print(x)
+
+num1 = input("enter num1 : ")
+num2 = input("enter num2 : ")
+print(num1+num2)
+print(int(num1)+int(num2))
+
+
