11d0058e67f7a8f4958505c1a7b71fff1427fe11
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-12 00:02:02 +0530

    12-09-2023

---
+++ nested_if.py
@@ -0,0 +1,18 @@
+
+x = 10
+y = 20
+z = 30
+
+if x>y : 
+    if x>z : 
+        print("x is maximu")
+    else :
+        print("z is maximum")
+        
+else :
+    if y>z :
+        print("y is maximum")
+    else :
+        print("z is maximum")
+        
+        
